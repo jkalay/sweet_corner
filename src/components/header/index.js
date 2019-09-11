07486dd42d9c7689689069e92f8437393e709a59
jkalay <jkalay@gmail.com>
2019-09-11 20:02:59 +0000

    Build Header component to design doc specifications. Add Nav component with ul list items having respective children Link components for routing. Add basic SASS styling.

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./header.scss";
+import Nav from "../nav";
+import Logo from "../../assets/images/logo.png";
 
 export default props => {
   return (
-    <div>
-      <h1 className="center">This is the Header</h1>
-    </div>
+    <header>
+      <div className="top-image"></div>
+      <h1 className="center">
+        <img src={Logo} />
+      </h1>
+      <h2 className="slogan center">We deliver cupcakes for the important events in your life!</h2>
+      <Nav />
+    </header>
   );
 }
