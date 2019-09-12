7ca35ba17004e56593349158c0839085097319a2
jkalay <jkalay@gmail.com>
2019-09-12 00:57:59 +0000

    Build Footer component to design doc specifications

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./footer.scss";
+import Phone from "../../assets/images/phone.png";
 
 export default props => {
+  let now = new Date();
+
   return (
-    <div>
-      <h1 className="center">This is the Footer</h1>
-    </div>
+    <footer>
+      <div className="footer-dots" />
+      <div className="footer-copyright">Copyright &copy; {now.getUTCFullYear()} Sweet Corner. All rights reserved.</div>
+      <div className="footer-contact">
+        <img src={Phone} /> 800 264 2099
+      </div>
+    </footer>
   );
 }
